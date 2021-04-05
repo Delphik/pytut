c94deb62b3c7322cf5fc2caa7901204e03fa690e
Delphik <max.henny@gmail.com>
2021-04-04 21:02:17 -0400

    Added Tutorial 7

---
+++ pytut_7_loops_iterations.py
@@ -0,0 +1,27 @@
+# https://www.youtube.com/watch?v=6iF8Xb7Z3wQ&list=PL-osiE80TeTt2d9bfVyTiXJA-UTHn6WwU&index=7
+
+nums = [1, 2, 3, 4, 5]
+x = 0
+
+for num in nums:
+    #if num == 3:
+       # print('Found!')
+        # break
+        #continue
+   for letter in 'abc':
+    print(num, letter)
+
+print()
+
+for i in range(1,11):
+    print(i)
+
+print()
+
+while True:
+    if x == 5:
+        break
+    print(x)
+    x += 1
+
+print()
